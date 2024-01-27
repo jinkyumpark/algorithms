25a93fdea6c719fc5b67753ac0fe081816842dae
Jinkyum Park <jinkyumpark@toss.im>
2024-01-27 14:22:55 +0900

    1월 13일 스터디 추가

--- dangsan_231202/HanoiTower.kt
+++ dangsan_231202/HanoiTower.kt
@@ -2,16 +2,19 @@ package dangsan_231202
 
 class HanoiTower {
     fun solution(n: Int): Array<IntArray> {
-        val moves = mutableListOf<IntArray>()
-        hanoi(n, 1, 2, 3, moves)
+        hanoi(n, start = 1, end = 3, auxiliary = 2)
         return moves.toTypedArray()
     }
 
-    private fun hanoi(n: Int, start: Int, auxiliary: Int, end: Int, moves: MutableList<IntArray>) {
+    private fun hanoi(n: Int, start: Int, end: Int, auxiliary: Int) {
         if (n == 0) return
 
-        hanoi(n - 1, start, end, auxiliary, moves)
+        hanoi(n - 1, start = start, end = auxiliary, auxiliary = end)
         moves.add(intArrayOf(start, end))
-        hanoi(n - 1, auxiliary, start, end, moves)
+        hanoi(n - 1, start = auxiliary, end = end, auxiliary = start)
+    }
+
+    companion object {
+        private val moves = mutableListOf<IntArray>()
     }
 }
